44f5bccffccd1049ce304b37d1bd147849bd0322
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2021-12-12 02:28:24 +0300

    Add files via upload

---
+++ PersonelTakipSistemi/PersonelTakipSistemi/Person.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonelTakipSistemi
+{
+
+    enum Statu
+    {
+        Yonetici,
+        Idari,
+        Isci
+    }
+    class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public Person(string name, string lastname, decimal startsalary, DateTime born, Statu pozisyon, DateTime startdate)
+        {
+            this.Name = name;
+            this.LastName = lastname;
+            this.StartSalary = startsalary;
+            this.Born = born;
+            this.Pozisyon = pozisyon;
+            this.StartDate = startdate;
+        }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName 
+        {
+            get
+            {
+                return Name + " " + LastName;
+            }
+        }
+        public decimal StartSalary { get; set; }
+
+        public DateTime Born { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int WorkYear 
+        {
+            get 
+            {
+                return DateTime.Now.Year - StartDate.Year;
+            }
+        }
+
+        public int Age 
+        {
+            get
+            {
+               return DateTime.Now.Year - Born.Year;
+            }
+        }
+
+        public decimal Salary
+        {
+            get 
+            {
+                if (WorkYear == 0)
+                {
+                    return StartSalary;
+                }
+                else if (WorkYear > 10) 
+                {
+                    return StartSalary + (StartSalary * (30M / 100M));
+                }
+                else if (WorkYear > 5)
+                {
+                    return StartSalary + (StartSalary * (20M / 100M));
+                }
+                else
+                {
+                    return StartSalary + (StartSalary * (10M / 100M));
+                }
+            }
+        }
+
+        public string BilgiGetir()
+        {
+            return FullName + " " + Age + " " + Salary + " " + WorkYear;
+        }
+
+        public Statu Pozisyon { get; set; }
+
+
+    }
+}
---
+++ PersonelTakipSistemi/PersonelTakipSistemi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonelTakipSistemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region bağımsızconstructoriçin
+            Person person1 = new Person();
+            person1.Name = "ali";
+            person1.LastName = "türkmen";
+            person1.Born = new DateTime(1996, 03, 17);
+            person1.Pozisyon = Statu.Isci;
+            person1.StartSalary = 5000M;
+            person1.StartDate = new DateTime(2015, 07, 29);
+            Console.WriteLine(person1.BilgiGetir());
+            #endregion
+
+            #region bağımlıconstructoriçin
+            Person person2 = new Person("ali", "türkmen", 30000M, new DateTime(1996, 03, 17), Statu.Yonetici, new DateTime(2018, 03, 17));
+            Console.WriteLine(person2.BilgiGetir()); 
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
